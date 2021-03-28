c0452bb00288ced7d1149d9a9160b96a80e7d1e6
Rivaldo1123 <ramanlewis@gmail.com>
2021-03-28 13:33:29 -0500

    up

--- js-index.js
+++ js-index.js
@@ -1,54 +1,55 @@
 var nameInput = document.getElementById("name-input"),
-	messageInput = document.getElementById("message-input");
+    messageInput = document.getElementById("message-input");
 
 function handleKeyUp(e) {
-	if (e.keyCode === 13) {
-		sendMessage();
-	}
+    if (e.keyCode === 13) {
+        sendMessage();
+    }
 }
+
 function sendMessage() {
-	var name = nameInput.value.trim(),
-		message = messageInput.value.trim();
+    var name = nameInput.value.trim(),
+        message = messageInput.value.trim();
 
-	if (!name)
-		return alert("Please fill in the name");
+    if (!name)
+        return alert("Please fill in the name");
 
-	if (!message)
-		return alert("Please write a message");
+    if (!message)
+        return alert("Please write a message");
 
-	var ajax = new XMLHttpRequest();
-	ajax.open("POST", "php-send-message.php", true);
-	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	ajax.send("name=" + name + "&message=" + message);
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST", "php-send-message.php", true);
+    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ajax.send("name=" + name + "&message=" + message);
 
-	messageInput.value = "";
+    messageInput.value = "";
 }
 
 // web sockets
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-var connection = new WebSocket('ws://localhost:8080');
+var connection = new WebSocket('ws://yes-p.herokuapp.com:8080');
 var connectingSpan = document.getElementById("connecting");
-connection.onopen = function () {
-	connectingSpan.style.display = "none";
+connection.onopen = function() {
+    connectingSpan.style.display = "none";
 };
-connection.onerror = function (error) {
-	connectingSpan.innerHTML = "Error occured";
+connection.onerror = function(error) {
+    connectingSpan.innerHTML = "Error occured";
 };
-connection.onmessage = function (message) {
-	var data = JSON.parse(message.data);
+connection.onmessage = function(message) {
+    var data = JSON.parse(message.data);
 
-	var div = document.createElement("div");
-	var author = document.createElement("span");
-		author.className = "author";
-		author.innerHTML = data.name;
-	var message = document.createElement("span");
-		message.className = "messsage-text";
-		message.innerHTML = data.message;
+    var div = document.createElement("div");
+    var author = document.createElement("span");
+    author.className = "author";
+    author.innerHTML = data.name;
+    var message = document.createElement("span");
+    message.className = "messsage-text";
+    message.innerHTML = data.message;
 
-	div.appendChild(author);
-	div.appendChild(message);
+    div.appendChild(author);
+    div.appendChild(message);
 
-	document.getElementById("message-box").appendChild(div);
+    document.getElementById("message-box").appendChild(div);
 
 }
